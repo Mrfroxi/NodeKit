933e626284adf3404554dd0259ce5c7892c06bc9
Mrfroxi <pilutikas340@gmail.com>
2025-12-09 08:59:56 +0300

    fix:clear project

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,7 @@ import express,{Request,Response} from 'express';
 const app = express();
 const port = process.env.PORT || 3003;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/',(req: Request,res: Response) =>{
-    res.sendStatus(203);
-})
-
-app.post('/test',(req: Request,res: Response) =>{
-    let body  = req.body
-    res.send(body);
-})
-
-
-app.put('/test/:id',(req: Request,res: Response) =>{
-    let params  = req.params
-    res.send(body);
-})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
